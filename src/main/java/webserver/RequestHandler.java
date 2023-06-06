50ad06eed23ede2c8e0f0a448f6d39cd14b7a12e
steadykingdev <steadykingdev@gmail.com>
2023-06-06 15:14:58 +0900

    회원가입 post 방식으로 request

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,6 +9,7 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -31,30 +32,59 @@ public class RequestHandler extends Thread {
             // line : GET /index.html HTTP/1.1
             String line = br.readLine();
 
-            String[] lineSplit = line.split("\\?");
+            log.info("line = {}", line);
 
             String[] tokens = line.split(" ");
+            String httpMethod = tokens[0];
             String url = tokens[1];
             if (line == null) {
                 return;
             }
 
+            log.debug("httpMethod = {}", httpMethod);
             log.debug("url = {}", url);
 
-            String[] urlSplit = url.split("\\?");
+            // get 회원가입
+//            String[] urlSplit = url.split("\\?");
 
-            if (urlSplit[0].equals("/user/create")) {
-                String[] queryString = urlSplit[1].split(" ");
-                Map<String, String> queryMap = HttpRequestUtils.parseQueryString(queryString[0]);
-                user = new User(queryMap.get("userId"), queryMap.get("password"), queryMap.get("name"), queryMap.get("email"));
-            }
+//            if (urlSplit[0].equals("/user/create")) {
+//                String[] queryString = urlSplit[1].split(" ");
+//                Map<String, String> queryMap = HttpRequestUtils.parseQueryString(queryString[0]);
+//                user = new User(queryMap.get("userId"), queryMap.get("password"), queryMap.get("name"), queryMap.get("email"));
+//            }
+//
+//            log.debug("get request user = {}", user);
 
-            log.debug("user = {}", user);
+            int contentLength = 0;
 
             // header
             while (!line.equals("")) {
                 line = br.readLine();
                 log.debug("header : {}", line);
+                if(line.contains("Content-Length")) {
+                    String[] contentLengthSplit = line.split(" ");
+                    contentLength = Integer.parseInt(contentLengthSplit[1]);
+                }
+            }
+
+            // post 회원가입
+            if (httpMethod.equals("POST") && url.equals("/user/create")) {
+                String body = IOUtils.readData(br, contentLength);
+                log.debug("body = {}", body);
+
+                Map<String, String> queryMap = HttpRequestUtils.parseQueryString(body);
+                user = new User(queryMap.get("userId"), queryMap.get("password"), queryMap.get("name"), queryMap.get("email"));
+
+                byte[] redirectPath = Files.readAllBytes(new File("./webapp/index.html").toPath());
+
+                DataOutputStream dos = new DataOutputStream(out);
+
+                response302Header(dos, redirectPath.length);
+                responseBody(dos, redirectPath);
+
+                log.debug("user = {}", user);
+
+                return;
             }
 
             // response body
@@ -72,6 +102,17 @@ public class RequestHandler extends Thread {
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
